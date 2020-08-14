f0257693a0b9ac78476ed370753207ae4872f806
Christie Yoon <cleex1999@gmail.com>
2020-08-14 20:49:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,9 +9,9 @@ class Patient
      @@all << self
   end
 
-  #def appointments(date, patient)
-      #Appointment.new(date, patient, self)
-  #end
+  def new_appointment(date, doctor)
+      Appointment.new(date, self, doctor)
+  end
 
   def self.all
     @@all
