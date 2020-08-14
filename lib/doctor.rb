7713142e755ae4441e72f5af1da4f9cc72aa762a
Christie Yoon <cleex1999@gmail.com>
2020-08-14 20:39:59 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,15 @@
+class Doctor
+
+   @@all = []
+
+   attr_accessor :name, :patient
+
+   def initialize(name)
+      @name = name
+      @@all << self
+   end
+
+   def self.all
+     @@all
+   end
+end
