b339d5e1f1e66fc27e5c820bb076794d5f806d17
Christie Yoon <cleex1999@gmail.com>
2020-08-14 20:47:44 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+     @name = name
+     @@all << self
+  end
+
+  #def appointments(date, patient)
+      #Appointment.new(date, patient, self)
+  #end
+
+  def self.all
+    @@all
+  end
+end
